84f325fc2db644ba929f5fc2fb9d0152646e957e
v4nixd <mishanikolchuk03@gmail.com>
2026-04-07 02:02:47 +0200

    Add config get_var & set_var functions

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+from typing import Any
 
 import yaml
 from dotenv import load_dotenv
@@ -26,7 +27,29 @@ class Config:
             Config._instance = Config()
         return Config._instance
 
+    def get_var(self, path: str) -> Any:
+        keys = path.split(".")
+        data = self.config
+        for key in keys:
+            if key not in data:
+                raise KeyError(f"Key '{key}' not found in config.")
+            data = data[key]
+        return data
+
+    def set_var(self, path: str, value) -> None:
+        self._set_value(self.config, path, value)
+        with open(Config.CONFIG_FILE, "w", encoding="utf-8") as f:
+            yaml.safe_dump(self.config, f)
+
     def get_env_var(self, envvar: str) -> str:
         if envvar not in os.environ:
             raise ValueError(f"Environment variable '{envvar}' not found.")
         return os.environ[envvar]
+
+    def _set_value(self, data, path, value) -> None:
+        keys = path.split(".")
+        for key in keys[:-1]:
+            if key not in data:
+                data[key] = {}
+            data = data[key]
+        data[keys[-1]] = value
